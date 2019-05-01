8ddb8312d15a3a4f1a56719498066742fc73b193
Dominic Michaud <dominic@ateliermade.ca>
2019-05-01 14:36:12 -0400

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+
+    $('.showcontent').click(function(event) {
+    	$('.hidden-progress-bar').addClass('active');
+    	$('.hidden-content-container').addClass('opened');
+    	setTimeout(function(){
+    		$('.hidden-content-container').find('.text-container,.close-btn').show().animate({opacity: 1,},500);
+    	}, 1000);
+    });
+
+    $('.close-btn').click(function(event) {
+    	$('.hidden-content-container').find('.text-container,.close-btn').animate({opacity: 0,},250, function(){
+    		$('.hidden-content-container').find('.text-container,.close-btn').hide();
+    		$('.hidden-content-container').removeClass('opened');
+    		$('.hidden-progress-bar').removeClass('active');
+    	});
+    });
+});
+
+function myResize() {
+    var bWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var bHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;   
+}
